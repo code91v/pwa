d61daab20b625a563801655eb9e345f3168d44ce
code91 <jose91.florez@gmail.com>
2020-01-08 13:31:22 -0400

    docs

---
+++ docs/precache-manifest.358a490f9a4bbe2cbe749f35ee9505d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e33a191cd186d29a33a",
+    "url": "/css/app.a510f641.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "923ee6cb962f7b77a2165300e6db606a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e33a191cd186d29a33a",
+    "url": "/js/app.ccafa5b9.js"
+  },
+  {
+    "revision": "5e727348b2da7f553c6c",
+    "url": "/js/chunk-vendors.b6b2dd8c.js"
+  },
+  {
+    "revision": "6c3f702b6a33169acd04beba4e260125",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
